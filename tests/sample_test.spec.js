46888249637e102eedd063a62dfeb0c5783590aa
atrinil <atrinil@gmail.com>
2025-12-07 11:39:17 +0530

    added to attach full-page screenshot to native playwright report

--- tests/sample_test.spec.js
+++ tests/sample_test.spec.js
@@ -1,7 +1,9 @@
-const { test } = require('@playwright/test');
+const { test,testInfo } = require('@playwright/test');
 
-test.only('sample test to launch sauce demo webapp', async ({browser}) => {
+test('@sauce sample test to launch sauce demo webapp', async ({browser}, testInfo) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://www.saucedemo.com/');
+    const screenshot = await page.screenshot();
+    await testInfo.attach('screenshot', {body:screenshot, contentType:'image/png'});
 });
