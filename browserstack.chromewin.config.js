22949ffa0ece5be8c9756e31f35536e25b41a0b6
atrinil <atrinil@gmail.com>
2025-12-14 20:16:17 +0530

    add world class to move browser creaion logic to, added browserstack config

---
+++ browserstack.chromewin.config.js
@@ -0,0 +1,15 @@
+const playwrightVersion = require('playwright/package.json').version;
+
+const capsChromeOsX = {
+    browser: 'chrome',
+    os: 'windows',
+    os_version: '11',
+    'browserstack.username': 'atrinilbal_moCzgQ',
+    'browserstack.accessKey': 'EYEsGefYFcrgY9GtiNjf',
+    'client.playwrightVersion': playwrightVersion,
+    projectName: 'playwrightwithjavascript',
+    buildName: 'Chrome-On-OsX',
+    "browserstack.local": false
+};
+
+module.exports = {capsChromeOsX};
---
+++ features/support/CustomWorld.js
@@ -0,0 +1,66 @@
+console.log('Loading Custom World file');
+
+const{ setWorldConstructor, World} = require('@cucumber/cucumber');
+const{ chromium, firefox, webkit, devices} = require('playwright');
+const { capsChromeOsX } = require('../../browserstack.chromeosx.config');
+const { capsChromeOsX } = require('../../browserstack.chromewin.config');
+
+
+class CustomWorld extends World {
+    constructor(options) {
+        super(options);
+        this.browser = null;
+        this.context = null;
+        this.page = null;
+        this.isMobile = null;
+        this.mobileDevice = null;
+    }
+
+    async launch() {
+        const browserName = process.env.BROWSER || 'chromium';
+
+        if(browserName.includes('-bs')) {
+            console.log('Connecting to Browserstack');
+            let wsEndpoint;
+            switch(browserName) {
+                case 'chromium-osx-bs': wsEndpoint = `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify(capsChromeOsX))}`;
+                case 'chormium-win-bs': wsEndpoint = `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify(capsChromeOsX))}`;
+                default: throw new Error(`Unknown BS browser: ${browserName}`);
+            }
+            this.browser = await chromium.connect({wsEndpoint});
+            this.context = await this.browser.newContext();
+            this.page = await this.context.newPage();
+            console.log('Connected to Browserstack');
+            return;
+        }
+
+        let browserType;
+        if(browserName.includes('firefox')) browserType = firefox;
+        else if (browserName.includes('webkit')) browserType = webkit;
+        else browserType = chromium;
+
+        if (browserName.includes('-mobile')) {
+            this.isMobile = true;
+            this.mobileDevice = devices[browserName === 'chromium-mobile' ? 'Galaxy S5' : 'iPhone 11'];
+            this.browser = await browserType.launch({headless:false});
+            this.context = await this.browser.newContext({...this.mobileDevice});
+        } else {
+            //desktop launch
+            this.browser= await browserType.launch({
+                headless: false,
+                args: ["--start-maximized"]
+            });
+            this.context = await this.browser.newContext({});
+        }
+        this.page = await this.context.newPage();
+        console.log('Local session started');
+    }
+
+    async close () {
+        if (this.connect) await this.context.close();
+        if (this.browser) await this.browser.close();
+    }
+}
+
+//Tell Cucumber to use this custom World class
+setWorldConstructor(CustomWorld);
